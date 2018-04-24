22503f616e4c4b92cd0aac975042ebc1c0261d42
Kevin Lai <kevin.km.lai@gmail.com>
2018-04-24 14:00:02 +1000

    budgetController component

--- app.js
+++ app.js
@@ -1,29 +1,136 @@
 var budgetController = (function () {
 
-})();
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        this.percentage = -1;
+    };
 
-var UIController = (function () {
+    Expense.prototype.calcPercentage = function (totalIncome) {
 
-    var DOMstrings = {
-        inputType: '.add__type',
-        inputDescription: 'add__description',
-        inputValue: 'add__value',
-        inputBtn: '.add__btn'
+        if (totalIncome > 0) {
+            this.percentage = Math.round(this.value / totalIncome * 100);
+        } else {
+            this.percentage = -1;
+        }
 
     };
+
+    Expense.prototype.getPercentage = function () {
+        return this.percentage;
+    }
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var calculateTotal = function (type) {
+        var sum = 0;
+        data.allItems[type].forEach(function (currentValue) {
+            sum += currentValue.value;
+        });
+        data.totals[type] = sum;
+    }
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+
+        totals: {
+            exp: 0,
+            inc: 0
+        },
+        budget: 0,
+        percentage: -1
+    };
+
 
     return {
-        getInput: function () {
+        addItem: function (type, des, val) {
+            var newItem;
+
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+
+            data.allItems[type].push(newItem);
+
+            return newItem;
+
+        },
+
+        deleteItem: function (type, id) {
+            var ids, index;
+
+            ids = data.allItems[type].map(function (currentValue) {
+                return currentValue.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+
+                data.allItems[type].splice(index, 1);
+            }
+        },
+
+        calculateBudget: function () {
+
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            data.budget = data.totals.inc - data.totals.exp;
+
+            if (data.totals.inc > 0) {
+                data.percentage = Math.round(data.totals.exp / data.totals.inc * 100);
+            } else {
+                data.percentage = -1;
+            }
+
+        },
+
+        calculatePercentages: function () {
+
+            data.allItems.exp.forEach(function (currentValue) {
+                currentValue.calcPercentage(data.totals.inc);
+            });
+
+        },
+
+        getPercentages: function () {
+            var allPerc = data.allItems.exp.map(function (currentValue) {
+                return currentValue.getPercentage()
+            });
+            return allPerc;
+        },
+
+        getBudget: function () {
             return {
-                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
-                description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
             };
         },
-        getDOMstrings: function () {
-            return DOMstrings
+
+        testing: function () {
+            console.log(data);
         }
-    };
+    }
 
 })();
 
